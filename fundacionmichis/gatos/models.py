931fe71a28ba8bf60b0dba8837b9bf4a4deb3fc8
PaoNeko1 <paola.arcos.c@gmail.com>
2020-10-22 19:14:32 -0300

    Creacion de modelo

--- fundacionmichis/gatos/models.py
+++ fundacionmichis/gatos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Gatitos(models.Model):
+    id=models.IntegerField(primary_key=True, help_text='Ingrese ID del gato')
+    nombre=models.TextField(max_length=50,help_text='Ingrese nombre del gato')
+    edad=models.IntegerField(help_text='Ingrese edad del gato')
+    carac=models.TextField(max_length=1000, help_text='Ingrse caracteristicas del gato')
